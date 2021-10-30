f24a9c5812eee657e3b5d7334646d34823e453a2
Garmash Alexander <garmashalexander19@gmail.com>
2021-10-30 13:30:51 +0200

    taskList

--- src/index.js
+++ src/index.js
@@ -1,57 +1,66 @@
-import _ from 'lodash';
-import Decimal from 'decimal.js';
-
 import './index.css';
 
-const form = document.querySelector('body');
-form.insertAdjacentHTML('beforeend', '<input type="text" id="first_operand"  name="fname"><br>');
-form.insertAdjacentHTML('beforeend', '<input type="text" id="second_operand" name="fname2"><br>');
-form.insertAdjacentHTML('beforeend', '<button type="button">Посчитать!</button>');
+//* ***************************************************************/
+/*
+ *GLOBALS VARIABLES
+ */
 
-const button = form.querySelector('button');
-button.addEventListener('click', () => {
-  const divs = document.body.querySelectorAll('div');
-  for (let i = 0; i < divs.length; i += 1) {
-    divs[i].remove();
-  }
-  const [fOp, sOp] = form.querySelectorAll('input');
-  let correct = true;
-  if (!Number(fOp.value)) {
-    correct = false;
-    fOp.insertAdjacentHTML(
-      'afterEnd',
-      `<div class="error-message">
-      <h1>Это не число</h1>
-    </div>`
-    );
-  }
-  if (!Number(sOp.value)) {
-    correct = false;
-    sOp.insertAdjacentHTML(
-      'afterEnd',
-      `<div class="error-message">
-      <h1>Это не число</h1>
-    </div>`
-    );
-  }
-  if (correct) {
-    const result = new Decimal(+fOp.value).plus(+sOp.value);
-    form.querySelector('button').insertAdjacentHTML(
-      'afterEnd',
-      `<div class="result">
-            <h1>${result}</h1>
-        </div>`
-    );
-  }
-});
+const btnSend = document.getElementById('btn-send');
+const textField = document.getElementById('text-work');
+const ul = document.getElementById('list-inline');
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+//* ***************************************************************/
+/*
+ *FUNCTIONS "PUSH/POP" WITHOUT LOCALSTORE
+ */
+function getJStore() {
+  return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')).sort() : [];
 }
 
-document.body.appendChild(component());
+function setJStore(list) {
+  return localStorage.setItem('tasks', JSON.stringify(list));
+}
+//* ***************************************************************/
+/*
+ *DRAW/ERASE LI ELEMENTS IN UL ELEMENT
+ */
+function drawList(list, elem) {
+  list.forEach(el => {
+    elem.insertAdjacentHTML('beforeend', `<li>${el}</li>`);
+  });
+}
+
+function eraseList() {
+  ul.querySelectorAll('li').forEach(el => el.remove());
+}
+
+/*
+ *ALL LOGIC
+ */
+function getList() {
+  if (textField.value) {
+    eraseList();
+    setJStore(getJStore().concat(textField.value));
+    drawList(getJStore(), ul);
+    textField.value = '';
+  }
+}
+
+drawList(getJStore(), ul);
+//* ***************************************************************/
+/*
+ *FOR ENTER
+ */
+const form = document.getElementById('form');
+
+form.onsubmit = () => {
+  getList();
+  return false;
+};
+//* ********************************************/
+/*
+ *SAME FOR BUTTON
+ */
+btnSend.addEventListener('click', () => {
+  getList();
+});
